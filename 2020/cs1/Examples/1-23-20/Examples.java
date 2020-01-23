40ba2b3a3b6b632e6b3d183a111d79026a552bf3
mja00 <me@mja00.dev>
2020-01-23 09:41:10 -0500

    Added first lecture examples

---
+++ 2020/cs1/Examples/1-23-20/Examples.java
@@ -0,0 +1,10 @@
+class Examples
+{
+	public static void main(String[] args)
+	{
+		System.out.println("5 + 2 = "+(5+2));
+		System.out.println(5-4*7+1);
+		System.out.println((5-4)*7+1);
+
+	}
+}
---
+++ 2020/cs1/Examples/1-23-20/FirstProgram.java
@@ -0,0 +1,13 @@
+class FirstProgram
+{
+	public static void main(String[] args)
+	{
+		//print means no new line
+		System.out.print("Hello World!");
+		//System.out.println("123");
+		/*
+		mdjk;54
+		
+		*/
+	}
+}
